ac36cf63be55d47332fa5e986037bba9624fd1a9
mmarbonillo <mbonillo95@gmail.com>
2019-09-28 13:20:13 +0200

    ftyjfjft

---
+++ src/main/java/Mqtt.java
@@ -0,0 +1,33 @@
+import org.eclipse.paho.client.mqttv3.*;
+
+public class Mqtt {
+	public static final String BROKER_URL = "tcp://192.168.43.215:1883";
+    private MqttClient client;
+
+    public Mqtt() {
+
+         String clientId = "demonio";
+         try {
+             client = new MqttClient(BROKER_URL, clientId);
+         }
+         catch (MqttException e) {
+             e.printStackTrace();
+             System.exit(1);
+         }
+    }
+
+    public void start() {
+
+         try {
+             client.setCallback(new SubscribeCallback());
+             client.connect();
+             client.subscribe("/sensorA/#");
+             client.subscribe("/sensorB/#");
+         }
+         catch (MqttException e) {
+             e.printStackTrace();
+             System.exit(1);
+         }
+
+    }
+}
