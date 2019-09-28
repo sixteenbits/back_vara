db46d7743466db9dad1d0382acc96d69706bf378
mmarbonillo <mbonillo95@gmail.com>
2019-09-28 15:44:28 +0200

    hecho

--- src/main/java/Mqtt.java
+++ src/main/java/Mqtt.java
@@ -1,34 +1,31 @@
 import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.internal.ClientComms;
+import org.eclipse.paho.client.mqttv3.internal.ClientState;
+import org.eclipse.paho.client.mqttv3.internal.MessageCatalog;
+import java.awt.Robot;
 
 public class Mqtt {
+	 static MqttClient client;
+	public static void main(String[] args) {
+		
+        String clientId = "cliente";
+        
+        try {
+            client = new MqttClient(BROKER_URL, clientId);
+            client.setCallback(new SubscribeCallback());
+            client.connect();
+            client.subscribe("jamtoday/vara");
+        }
+        catch (MqttException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        
+	}
 	public static final String BROKER_URL = "tcp://192.168.43.215:1883";
-    private MqttClient client;
+    
 
     public Mqtt() {
-
-         String clientId = "demonio";
-         try {
-             client = new MqttClient(BROKER_URL, clientId);
-         }
-         catch (MqttException e) {
-             e.printStackTrace();
-             System.exit(1);
-         }
-    }
-
-    public void start() {
-
-         try {
-             client.setCallback(new SubscribeCallback());
-             client.connect();
-             client.subscribe("derecha");
-             client.subscribe("izquierda");
-             client.subscribe("golpe");  
-         }
-         catch (MqttException e) {
-             e.printStackTrace();
-             System.exit(1);
-         }
 
     }
 }
--- src/main/java/SubscribeCallback.java
+++ src/main/java/SubscribeCallback.java
@@ -1,12 +1,37 @@
 import org.eclipse.paho.client.mqttv3.*;
 import java.lang.Throwable;
+import java.awt.AWTException;
+import java.awt.Robot;
 
 public class SubscribeCallback implements MqttCallback {
 
      public void connectionLost(Throwable cause) {}
 
      public void messageArrived(String topic, MqttMessage message) {
+    	 Robot robot = null;
+    	 try {
+			robot = new Robot();
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	 
           System.out.println(topic + ": " + message.toString());
+          char tecla = message.toString().charAt(0);
+          switch(tecla) {
+          //IZQUIERDA
+          case 'l':
+        	  robot.keyPress(65);
+        	  break;
+        	  //GOLPE
+          case 'v':
+        	  robot.keyPress(83);
+        	  break;
+        	  //DERECHA
+          case 'r':
+        	  robot.keyPress(68);
+        	  break;
+          }
      }
 
      public void deliveryComplete(IMqttDeliveryToken token) {}
