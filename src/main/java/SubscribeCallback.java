ea7d9387fbc2da89b4ecdbb9089df583504637cf
mmarbonillo <mbonillo95@gmail.com>
2019-09-28 13:36:43 +0200

    asdf

--- src/main/java/SubscribeCallback.java
+++ src/main/java/SubscribeCallback.java
@@ -3,15 +3,12 @@ import java.lang.Throwable;
 
 public class SubscribeCallback implements MqttCallback {
 
-     @Override
      public void connectionLost(Throwable cause) {}
 
-     @Override
      public void messageArrived(String topic, MqttMessage message) {
           System.out.println(topic + ": " + message.toString());
      }
 
-     @Override
      public void deliveryComplete(IMqttDeliveryToken token) {}
 
 }
